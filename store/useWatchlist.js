d22cc4e4f64e29b61c0625cb371864042570da11
hiramAcevedo <hiram.acevedo.lopez@gmail.com>
2025-05-05 10:22:55 -0700

    Fijar bitcoin, ethereum y dogecoin como monedas por defecto

--- store/useWatchlist.js
+++ store/useWatchlist.js
@@ -2,7 +2,7 @@
 import { create } from 'zustand';
 
 export const useWatchlist = create((set) => ({
-    coins: ['bitcoin'], // estado inicial con “bitcoin”
+    coins: ['bitcoin', 'ethereum', 'dogecoin'], // 3 monedas por defecto
     addCoin: (id) =>
         set((state) => ({
         coins: state.coins.includes(id)
